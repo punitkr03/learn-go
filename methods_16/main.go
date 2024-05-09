ed1146d9d3b91bfa66a4ff33471fe10c6b021fe3
punitkr03 <tagpunitkumar@gmail.com>
2024-05-09 17:09:33 +0530

    copy value pass

--- methods_16/main.go
+++ methods_16/main.go
@@ -9,6 +9,7 @@ func main() {
 	user_1 := User{"Punit", "abc@abc.com", true, 20}
 	fmt.Printf("User details are %+v\n", user_1)
 	user_1.GetStatus()
+	user_1.NewMail()
 }
 
 type User struct {
@@ -20,4 +21,10 @@ type User struct {
 
 func (u User) GetStatus() {
 	fmt.Println("Is user active; ", u.Status)
+}
+
+func (u User) NewMail() {
+	u.Email = "test@test.com"
+	//This makes a copy and passes the value.
+	fmt.Println("Email of this user is ", u.Email)
 }
