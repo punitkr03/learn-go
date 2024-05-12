108ce03a36f7cdc267785db4c5c0f1d0533d11ec
punitkr03 <tagpunitkumar@gmail.com>
2024-05-13 01:41:16 +0530

    Refactor URL parsing and add query parameter handling

--- web/urls_20/main.go
+++ web/urls_20/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net/url"
-	"strings"
+	// "strings"
 )
 
 const myurl = "https://localhost:3000/learn?coursename=react&payment_id=hello&payment_id=skfjh"
@@ -19,10 +19,28 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(result.Scheme)
-	fmt.Println(result.Host)
-	fmt.Println(result.Path)
-	fmt.Println(result.Port())
-	fmt.Println(result.RawQuery)
-	fmt.Println(strings.Split(result.RawQuery, "&"))
+	// fmt.Println(result.Scheme)
+	// fmt.Println(result.Host)
+	// fmt.Println(result.Path)
+	// fmt.Println(result.Port())
+	// fmt.Println(result.RawQuery)
+	// fmt.Println(strings.Split(result.RawQuery, "&"))
+
+	qparams := result.Query()
+	fmt.Printf("The type of value paramas are : %T\n", qparams)
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams {
+		fmt.Println("Params is: ", val)
+	}
+
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "localhost:3000",
+		Path:    "/learn",
+		RawPath: "user=punit",
+	}
+
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
 }
