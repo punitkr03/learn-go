ece033770d41b2595b0c4c99f7409edc38719bcb
punitkr03 <tagpunitkumar@gmail.com>
2024-05-14 22:02:59 +0530

    Get requests

---
+++ web/webreqverbs_21/go.mod
@@ -0,0 +1,3 @@
+module mywebverbs
+
+go 1.22.2
---
+++ web/webreqverbs_21/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Web verbs in go.")
+	PerformGetRequest()
+}
+
+func PerformGetRequest() {
+	const myurl = "http://localhost:3000/get"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	fmt.Println("Status Code: ", response.StatusCode)
+	fmt.Println("Content length is ", response.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := io.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("Bytecount is : ", byteCount)
+	fmt.Println(responseString.String())
+
+	// fmt.Println(string(content))
+}
